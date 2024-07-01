ea2430279639ce8a65a6d1db4f8b69af1748b066
msunni6197 <msunni6197@gmail.com>
2024-07-01 23:48:11 +0530

    Js_23:48

---
+++ JavaScript/Expense Tracker Application/addExpense.js
@@ -0,0 +1,10 @@
+expenses=[]
+categories=[]
+
+function addExpense() {
+    const descriptionInput = document.getElementById("descriptionInput");
+    const categoryInput = document.getElementById("categoryInput");
+    let description = descriptionInput.value.trim();
+    let category = Number (categoryInput.value.trim());
+    
+}
